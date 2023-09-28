3acbcd8493d1c8ba82a125aefd2f7475f1834357
Remon Dollenkamp <remon2004@outlook.com>
2023-09-28 15:34:52 +0200

    kapotte component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import ScrumTeamList from './components/ScrumTeamList.vue';
+import 'bootstrap';
+
+const app = createApp({});
+
+app.component('ScrumTeamList', ScrumTeamList);
+
+app.mount('#scrumteams');
