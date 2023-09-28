ff85edec1dcc86248dad2b6cc03828faa60a710e
Tvs9999 <tvanspanning@gmail.com>
2023-09-28 21:19:56 +0200

    few style changes

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,7 +9,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    
+    @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
     <title>Scrumteamsoverzicht</title>
 </head>
 
