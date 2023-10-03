84c3ef2489e27a03ed5f1caebcebf2c62e78ad0d
Tvs9999 <tvanspanning@gmail.com>
2023-10-03 11:46:07 +0200

    toggle

--- resources/js/components/ScrumTeamList.vue
+++ resources/js/components/ScrumTeamList.vue
@@ -1,27 +1,27 @@
 <template>
-  <div v-for="classData in classes" :key="classData.id" class="class" :class="{'active': !areAllUsersPresent('class', classData.id) || classStates[classData.id]}">
+  <div v-for="classData in classes" :key="classData.id" class="class" :class="{ 'active': !areAllUsersPresent('class', classData.id) }" :data-id="classData.id">
     <div class="top">
       <h2>{{ classData.name }}</h2>
       <div class="buttons">
         <div :class="{ 'present': areAllUsersPresent('class', classData.id), 'absent': !areAllUsersPresent('class', classData.id) }">
           <i :class="{ 'fa-solid fa-check': areAllUsersPresent('class', classData.id), 'fa-solid fa-xmark': !areAllUsersPresent('class', classData.id) }"></i>
         </div>
-        <!-- Bind click event to toggleClassCollapse method for classes -->
-        <div class="fold-btn" :class="{ 'active': classStates[classData.id] }" @click="toggleClassCollapse('class', classData.id)">
-            <i class="fa-solid fa-chevron-down"></i>
+        <!-- Bind click event to toggle 'active' class for classes -->
+        <div class="fold-btn" @click="toggleClass('class', classData.id)" :class="{ 'active': !areAllUsersPresent('class', classData.id) }">
+          <i class="fa-solid fa-chevron-down"></i>
         </div>
       </div>
     </div>
     <div class="scrumteams">
-      <div v-for="team in classData.scrumteams" :key="team.id" class="scrumteam" :class="{'active': !areAllUsersPresent('scrumteam', team.id) || scrumteamStates[classData.id][team.id]}">
+      <div v-for="team in classData.scrumteams" :key="team.id" class="scrumteam" :class="{ 'active': !areAllUsersPresent('scrumteam', team.id) }" :data-id="team.id">
         <div class="top">
           <h3>{{ team.name }}</h3>
           <div class="buttons">
             <div :class="{ 'present': areAllUsersPresent('scrumteam', team.id), 'absent': !areAllUsersPresent('scrumteam', team.id) }">
               <i :class="{ 'fa-solid fa-check': areAllUsersPresent('class', classData.id), 'fa-solid fa-xmark': !areAllUsersPresent('class', classData.id) }"></i>
             </div>
-            <!-- Bind click event to toggleClassCollapse method for scrumteams -->
-            <div class="fold-btn" :class="{ 'active': scrumteamStates[classData.id][team.id] }" @click="toggleClassCollapse('scrumteam', team.id, classData.id)">
+            <!-- Bind click event to toggle 'active' class for scrumteams -->
+            <div class="fold-btn" @click="toggleClass('scrumteam', team.id)" :class="{ 'active': !areAllUsersPresent('class', classData.id) }">
               <i class="fa-solid fa-chevron-down"></i>
             </div>
           </div>
@@ -43,8 +43,6 @@
   </div>
 </template>
 
-
-
 <script>
 import { defineComponent } from 'vue';
 
@@ -52,30 +50,6 @@ export default defineComponent({
   name: 'ScrumTeamList',
   props: {
     classes: Array,
-    // scrumteams: Array,
-    // scrumteamuser: Array,
-    // students: Array,
-  },
-  data() {
-    return {
-      classStates: {},
-      scrumteamStates: {},
-    };
-  },
-  mounted() {
-    if (this.classes) {
-      this.classes.forEach(classData => {
-        console.log('classData:', classData); // Log classData to check its structure
-        this.classStates[classData.id] = false; // Initially closed
-
-        // Initialize scrumteamStates for each class
-        this.scrumteamStates[classData.id] = {};
-        classData.scrumteams.forEach(team => {
-          console.log('team:', team); // Log team to check its structure
-          this.scrumteamStates[classData.id][team.id] = false; // Initially closed for each scrum team
-        });
-      });
-    }
   },
   methods: {
     areAllUsersPresent(type, id) {
@@ -98,37 +72,24 @@ export default defineComponent({
 
       return false;
     },
-
-
-    // ... other methods ...
-    toggleClassCollapse(type, id, classId) {
-      console.log('Toggle clicked for', type, id);
-      if (type === 'class') {
-        // Toggle the class state
-        this.classStates[id] = !this.classStates[id];
-      } else if (type === 'scrumteam') {
-        // Toggle the scrumteam state for the specific class and team
-        this.scrumteamStates[classId][id] = !this.scrumteamStates[classId][id];
+    toggleClass(type, id) {
+      const clickedButton = event.target.closest('.fold-btn');
+      if (clickedButton) {
+        clickedButton.classList.toggle('active');
       }
-      console.log('Updated classStates:', this.classStates);
-      console.log('Updated scrumteamStates:', this.scrumteamStates);
+
+      const element = document.querySelector(`.${type}[data-id="${id}"]`);
+      if (element) {
+        element.classList.toggle('active');
+      }
     },
-  }
-
-
-    //   return notAllMembersPresent;
-    // },
-    // getClassIcon(teamId) {
-    //   // Determine the icon class based on the check
-    //   return this.allMembersPresent(teamId) ? 'fa-solid fa-xmark text-danger' : 'fa-solid fa-check-circle text-success';
-    // },
-    // getTeamsByClass(classId) {
-    //   // Filter scrum teams based on the class ID
-    //   return this.scrumteams.filter(a => a.class_id === classId);
-    // },
-    // getTeamUsers(teamId) {
-    //   // Filter team users based on the team ID
-    //   return this.scrumteamuser.filter(user => user.scrumteam_id === teamId);
-    // },
+  },
 });
 </script>
+
+<style>
+/* Define your 'active' class styles here */
+.active {
+  /* Your 'active' class styles */
+}
+</style>
